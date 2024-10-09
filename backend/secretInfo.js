6ccd3eed27afc8c3acbed561a57060d03a6303db
Daniel <126022409+DanielGonzalezGil@users.noreply.github.com>
2024-10-09 13:57:56 -0400

    traking secretInfo.js

---
+++ backend/secretInfo.js
@@ -0,0 +1,5 @@
+// /Users/danielgonzalez/Desktop/CodeTheDream/CTD-API-Project/backend/secretInfo.js
+
+const apiKey ="live_6r30m2RlNgz3m3CQqmZQA1CCwksP2zV4QXZPRdSjOx2JCx0OGsMDRrzxyR85YYiw";
+
+export {apiKey};
